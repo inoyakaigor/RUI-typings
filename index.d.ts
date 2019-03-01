ecce35487f4b19994c3a461ced809ce3b331568d
Игорь Звягинцев <i.zvyagintsev@rambler-co.ru>
2019-03-01 14:48:27 +0300

    Фиксы и типизация для чекбокса

--- index.d.ts
+++ index.d.ts
@@ -1,59 +1,79 @@
 import {Component} from 'react'
 
-export namespace RamblerUI {
-    class Avatar extends Component<any, any> {}
+declare namespace RamblerUI {
+    export type Tvariation = 'regular' | 'awesome'
+    export type Tsize = 'small' | 'medium'
+    export type TiconPosition = 'left' | 'right'
 
-    class Button extends Component<any, any> {}
+    export class Avatar extends Component<any, any> {}
 
-    class Calendar extends Component<any, any> {}
+    export class Button extends Component<any, any> {}
 
-    class Checkbox extends Component<any, any> {}
+    export class Calendar extends Component<any, any> {}
 
-    class ComplexSearch extends Component<any, any> {}
+    export interface CheckboxProps {
+        name?: string
+        disabled?: boolean
+        className?: string
+        style?: object
+        iconPosition?: TiconPosition
+        checked?: boolean
+        indeterminate?: boolean
+        checkboxStyle?: object
+        checkboxclassName?: string
+        labelStyle?: object
+        labelclassName?: string
+        variation?: Tvariation
+        size?: Tsize
+        onCheck?: () => any
+    }
+    export class Checkbox extends Component<CheckboxProps, any> {}
 
-    class Dropdown extends Component<any, any> {}
+    export class ComplexSearch extends Component<any, any> {}
 
-    class FieldGroup extends Component<any, any> {}
+    export class Dropdown extends Component<any, any> {}
 
-    class Hint extends Component<any, any> {}
+    export class FieldGroup extends Component<any, any> {}
 
-    class IconButton extends Component<any, any> {}
+    export class Hint extends Component<any, any> {}
 
-    class Input extends Component<any, any> {}
+    export class IconButton extends Component<any, any> {}
 
-    class Loader extends Component<any, any> {}
+    export class Input extends Component<any, any> {}
 
-    class Menu extends Component<any, any> {}
+    export class Loader extends Component<any, any> {}
 
-    class Notification extends Component<any, any> {}
+    export class Menu extends Component<any, any> {}
 
-    class Pagination extends Component<any, any> {}
+    export class Notification extends Component<any, any> {}
 
-    class Popup extends Component<any, any> {}
+    export class Pagination extends Component<any, any> {}
 
-    class Radio extends Component<any, any> {}
+    export class Popup extends Component<any, any> {}
 
-    class Select extends Component<any, any> {}
+    export class Radio extends Component<any, any> {}
 
-    class SideNav extends Component<any, any> {}
+    export class Select extends Component<any, any> {}
 
-    class Snackbar extends Component<any, any> {}
+    export class SideNav extends Component<any, any> {}
 
-    class Spinner extends Component<any, any> {}
+    export class Snackbar extends Component<any, any> {}
 
-    class Stepper extends Component<any, any> {}
+    export class Spinner extends Component<any, any> {}
 
-    class Switcher extends Component<any, any> {}
+    export class Stepper extends Component<any, any> {}
 
-    class Tabs extends Component<any, any> {}
+    export class Switcher extends Component<any, any> {}
 
-    class TagsInput extends Component<any, any> {}
+    export class Tabs extends Component<any, any> {}
 
-    class Textarea extends Component<any, any> {}
+    export class TagsInput extends Component<any, any> {}
 
-    class Toggle extends Component<any, any> {}
+    export class Textarea extends Component<any, any> {}
 
-    class Tooltip extends Component<any, any> {}
+    export class Toggle extends Component<any, any> {}
+
+    export class Tooltip extends Component<any, any> {}
 
     type TypographyType = 'h1' | 'h2' | 'h3' | 'text' | 'quote' | 'epigraph' | 'source' | 'timestamp' | 'description' | 'galleryDescription' | 'photoSource' | 'list'
 
@@ -64,13 +84,15 @@ export namespace RamblerUI {
         children: React.ReactChild
     }
 
-    class Typography extends Component<TypographyProps, any> {}
+    export class Typography extends Component<TypographyProps, any> {}
 
     interface ListProps {
         numbered: boolean
     }
 
-    class List extends Component<ListProps, any> {}
+    export class List extends Component<ListProps, any> {}
 
-    class Icons extends Component<any, any> {}
+    export class Icons extends Component<any, any> {}
 }
+
+export = RamblerUI
